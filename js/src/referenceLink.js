31094c32209405a523ef26d39e2a44aa1890f7f7
scruffian <ben@scruffian.com>
2012-08-23 17:20:01 +0100

    new mobile friendly version with lots of bugs

--- js/src/referenceLink.js
+++
@@ -1,8 +0,0 @@
-/*global define, require, debug
-define(['jquery', 'jquery-mobile', 'ba-debug'], function ($) {
-	"use strict";
-	$(document).on('click', '.referenceLink', function () {
-		$('.selected-reference-link').removeClass('selected-reference-link');
-		$(this).closest('li').addClass('selected-reference-link');
-	});
-});*/
--- js/tests/keyboardShortcuts.js
+++
@@ -1,9 +0,0 @@
-/*global define, $, module, test, ok, debug */
-define(['../external/qunit', 'ba-debug', 'src/keyboardShortcuts'], function () {
-	"use strict";
-	module("keyboardShortcuts");
-
-	test("goto", function () {
-		ok();
-	});
-});
