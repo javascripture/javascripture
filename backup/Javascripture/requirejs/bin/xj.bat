55ba09dbf3da8ec60beeff7490594a48b690325f
scruffian <ben@scruffian.com>
2012-08-23 17:37:13 +0100

    deleting backup

--- backup/Javascripture/requirejs/bin/xj.bat
+++
@@ -1,2 +0,0 @@
-set MYDIR=%~dp0
-java -classpath %MYDIR%..\build\lib\rhino\js.jar org.mozilla.javascript.tools.shell.Main %MYDIR%x.js %MYDIR% %*
--- backup/Javascripture/requirejs/bin/xjdebug.bat
+++
@@ -1,2 +0,0 @@
-set MYDIR=%~dp0
-java -classpath %MYDIR%..\build\lib\rhino\js.jar org.mozilla.javascript.tools.debugger.Main %MYDIR%x.js %MYDIR% %*
