edcd45f4e8f96aaab678480729e73541c7cd4463
scruffian <ben@scruffian.com>
2013-01-22 19:35:49 +0000

    literal translation for hebrew and fixing families for words with more than one lemma

---
+++ js/src/translateLiterally.js
@@ -0,0 +1,37 @@
+/*global define, debug*/
+define(['jquery', 'literalTranslation'], function ($, literalTranslation) {
+	"use strict";
+	var translateLiterally = {
+		getWord: function (word) {
+			var literalWord;
+			if (typeof(word) === 'object') {
+				if (word.morph) {
+					literalWord = literalTranslation[word.lemma][word.morph];
+				} else {
+					literalWord = literalTranslation[word.lemma];
+				}
+			} else {
+				literalWord = literalTranslation[word];
+			}
+			if (literalWord !== undefined && typeof(literalWord) === 'object') { //need better way to test if is an object
+				//get first item in object
+				$.each(literalWord, function (morph, translation) {
+					literalWord = translation;
+					var first;
+					for (var i in literalWord) {
+					    if (literalWord.hasOwnProperty(i) && typeof(i) !== 'function') {
+							first = literalWord[i];
+							break;
+						}
+					}
+					//stop loop here;
+				});
+			}
+			if (literalWord === undefined) {
+				literalWord = '';
+			}
+			return literalWord;
+		}
+	};
+	return translateLiterally;
+});
