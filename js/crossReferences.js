a0b66b0311b1cb994debc0e2bdfd201fa2e3fd4f
Ben Dwyer <ben@scruffian.com>
2014-01-23 07:46:12 +0000

    fixing syntax issues with JSHint

--- js/crossReferences.js
+++ js/crossReferences.js
@@ -1,11 +1,10 @@
-/*global bible crossReferences*/
+/*global bible, crossReferences*/
 $( document ).on('click', '#verse .wrapper li', function() {
-
 	var idToUrl = function ( id ) {
 		var bookId = bible.getBookId( id ),
 		    referenceArray = id.split( '_' ),
 		    url = '#';
-		console.log( bookId );
+
 		if ( bookId ) {
 			url += 'book=' + bible.Data.books[ bookId - 1 ][ 0 ];
 		}
@@ -20,7 +19,7 @@ $( document ).on('click', '#verse .wrapper li', function() {
 	var idToPrettyReference = function( id ) {
 		var idArray = id.split( '_' );
 		return idArray[0] + ' ' + idArray[1] + ':' + idArray[2];
-	}
+	};
 	var book = $( this ).closest( '.reference' ).data( 'book' ),
 	    bookId = bible.getBookId( book ),
 	    chapter = $( this ).closest( '.reference' ).data( 'chapter' ),
@@ -28,16 +27,14 @@ $( document ).on('click', '#verse .wrapper li', function() {
 //	    chapterAndVerse = this.id.substr(this.id.indexOf( '_' ), this.id.length),
 //	    reference = bible.Data.books[bookId - 1][1] + chapterAndVerse.replace(/_/gi, '.'),
 	    reference = bible.Data.books[bookId - 1][1] + '.' + chapter + '.' + verse;
-	    console.log( bookId );
-	    console.log( reference );
-	    crossReferencesMarkup = '<div class="crossReferences">Cross references for <a href="#book=' + book + '&chapter=' + chapter + '&verse=' + verse + '">' + idToPrettyReference( this.id ) + '</a>:<br>';
-	console.log(crossReferences[ reference ] );
+
+	    var crossReferencesMarkup = '<div class="crossReferences">Cross references for <a href="#book=' + book + '&chapter=' + chapter + '&verse=' + verse + '">' + idToPrettyReference( this.id ) + '</a>:<br>';
+
 	if ( crossReferences[ reference ] ) {
 		$.each( crossReferences[ reference ], function( key, referenceString ) {
 			var referenceArray = referenceString.split('.'),
 			    bookId = bible.getBookId( referenceArray[0] );
 
-console.log(bookId);
 			crossReferencesMarkup += '<a href="#book=' + bible.Data.books[bookId - 1][0] + '&chapter=' + referenceArray[1] + '&verse=' + referenceArray[2] + '">' + referenceString + '</a> ';
 		} );
 	} else {
