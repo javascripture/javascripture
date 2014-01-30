2ed01fc1c91790d7422bcc5b25e69d6475a3b037
Ben Dwyer <ben@scruffian.com>
2014-01-30 17:09:33 +0200

    reordering scripts

--- js/events.js
+++ js/events.js
@@ -105,22 +105,6 @@
 			wordTree($('#reverseRootStrongsNumber').val());
 			return false;
 		});
-		/*Color Picker*/
-		$('#colorSelector').ColorPicker({
-			color: '#0000ff',
-			onShow: function (colpkr) {
-				$(colpkr).fadeIn(500);
-				return false;
-			},
-			onHide: function (colpkr) {
-				$(colpkr).fadeOut(500);
-				return false;
-			},
-			onChange: function (hsb, hex, rgb) {
-				$('#colorSelector div').css('backgroundColor', '#' + hex);
-				$('#colorFormColor').val('#'+hex).change();
-			}
-		});
 
 		$('.findRareWords').click(function(){
 			findRareWords($('#maximumNumberOfUses').val());
