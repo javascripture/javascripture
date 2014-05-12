e2ee473656d7c9d05a82530a018c9d6089cc3f22
Ben Dwyer <ben@scruffian.com>
2014-05-12 18:26:04 +0100

    passing JSHint

--- js/crossReferences.js
+++ js/crossReferences.js
@@ -1,36 +1,17 @@
-/*global bible, crossReferences*/
+/*global bible, crossReferences, javascripture*/
 $( document ).on('click', '#verse .wrapper li', function() {
-	var idToUrl = function ( id ) {
-		var bookId = bible.getBookId( id ),
-		    referenceArray = id.split( '_' ),
-		    url = '#';
-
-		if ( bookId ) {
-			url += 'book=' + bible.Data.books[ bookId - 1 ][ 0 ];
-		}
-		if ( referenceArray[ 1 ] ) {
-			url += '&chapter=' + referenceArray[ 1 ];
-		}
-		if ( referenceArray[ 2 ] ) {
-			url += '&verse=' + referenceArray[ 2 ];
-		}
-		return url;
-	};
 	var idToPrettyReference = function( id ) {
 		var idArray = id.split( '_' );
 		return idArray[0] + ' ' + idArray[1] + ':' + idArray[2];
 	};
-	var $reference = $( this ).closest( '.reference' );
+	var $reference = $( this ).closest( '.reference' ),
 		book = $reference.data( 'book' ),
 	    bookId = bible.getBookId( book ),
 	    chapter = $reference.data( 'chapter' ),
 		verse = $( this ).data( 'verse' ),
-//	    chapterAndVerse = this.id.substr(this.id.indexOf( '_' ), this.id.length),
-//	    reference = bible.Data.books[bookId - 1][1] + chapterAndVerse.replace(/_/gi, '.'),
 	    reference = bible.Data.books[bookId - 1][1] + '.' + chapter + '.' + verse;
-
 	    var crossReferencesMarkup = '<div class="crossReferences content-padding">Cross references for <a href="#book=' + book + '&chapter=' + chapter + '&verse=' + verse + '">' + idToPrettyReference( this.id ) + '</a>:<br>';
-		console.log( reference );
+
 	if ( crossReferences[ reference ] ) {
 		$.each( crossReferences[ reference ], function( key, referenceString ) {
 			var referenceArray = referenceString.split('.'),
