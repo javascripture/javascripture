dd581beb9d53bba0808b664a3f1a39f02950525c
Ben Dwyer <ben@scruffian.com>
2014-10-26 19:06:41 +0000

    fixing touch events on the reference picker

--- js/bookSelect.js
+++ js/bookSelect.js
@@ -1,8 +1,12 @@
-$('.bookSelect a').bind( 'mousemove touchmove', function(event) {
-	var numberOfChapters = parseInt( $(this).data( 'chapters' ), 10 ),
-	    position = event.clientX - $(this).offset().left,
-	    spacing =  $(this).width() / numberOfChapters,
-	    chapter = Math.ceil( position / spacing );
+$('.bookSelect a').bind( 'mousemove touchmove', function( event ) {
+	var numberOfChapters = parseInt( $(this).data( 'chapters' ), 10 );
+	var position = event.clientX - $(this).offset().left;
+
+	if ( ! event.clientX ) {
+		var position = event.originalEvent.touches[0].clientX - $(this).offset().left;
+	}
+	var spacing =  $(this).width() / numberOfChapters;
+	var chapter = Math.ceil( position / spacing );
 	if ( isNaN( chapter ) ) {
 		return false;
 	}
