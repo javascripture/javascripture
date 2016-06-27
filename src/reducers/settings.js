3b26242bc8a6b03abcd9871988deb709765423f3
Ben Dwyer <ben@scruffian.com>
2016-06-27 15:01:15 +0100

    Use a reducer for Settings

---
+++ src/reducers/settings.js
@@ -0,0 +1,24 @@
+const initialState = {
+	fontSize: "100%",
+	fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif",
+	highlightWordsWith: "same",
+	referencePicker: "select",
+	subdue: ".5",
+	type: "SETTINGS_CHANGE"
+}
+
+export default ( state = initialState, action ) => {
+	let settings;
+	switch ( action.type ) {
+		case 'SETTINGS_CHANGE':
+			settings = Object.assign( {}, state, action );
+			break;
+
+		default:
+			settings = state;
+			break;
+	}
+
+	javascripture.state.settings = settings;
+	return settings;
+}
