ae0fa1acc335698160befac1207a1f079469728e
Ben Dwyer <ben@bens-mbp.lan>
2016-12-26 17:23:48 +0000

    Fix keyboard shortcuts

--- src/components/trays/tray-list.js
+++ src/components/trays/tray-list.js
@@ -11,6 +11,9 @@ import SearchTray from './search';
 import BookmarksTray from './bookmarks';
 import SettingsTray from './settings';
 
+// Component variables
+let lastTimeStamp = 0;
+
 function getComponent( componentString ) {
 	switch ( componentString ) {
 		case 'WordTray':
@@ -45,14 +48,41 @@ class TrayList extends React.Component{
 		}
 	}
 
+	goToChapter( event, combo ) {
+		const currentTimeStamp = Math.floor( event.timeStamp ),
+			currentReference = javascripture.modules.reference.getReferenceFromHash(),
+			bookId = bible.getBookId( currentReference.book );
+
+		let chapterToGoTo = combo;
+		if ( currentTimeStamp - lastTimeStamp < 750) {
+			chapterToGoTo = currentReference.chapter + combo;
+		}
+
+		if ( bible.Data.verses[bookId - 1][ chapterToGoTo - 1] ) {
+			var newReference = currentReference;
+			newReference.chapter = chapterToGoTo;
+			window.location.hash = javascripture.modules.reference.createReferenceLink( newReference );
+		}
+
+		lastTimeStamp = currentTimeStamp;
+	}
+
+	goToReference() {
+		$( '#goToReference' ).val( '' ).focus();
+	}
+
 	componentDidMount() {
 		mousetrap.bind( [ '=' ], () => this.goToNextCurrentVerse( false ) );
 		mousetrap.bind( [ '-' ], () => this.goToPreviousCurrentVerse( false ) );
+		mousetrap.bind( [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ], this.goToChapter );
+		mousetrap.bind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], this.goToReference );
 	}
 
 	componentWillUnmount() {
 		mousetrap.unbind( [ '=' ], () => this.goToNextCurrentVerse( false ) );
 		mousetrap.unbind( [ '-' ], () => this.goToPreviousCurrentVerse( false ) );
+		mousetrap.unbind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], this.goToReference );
+
 	}
 
 	render() {
