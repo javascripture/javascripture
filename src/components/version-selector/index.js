2e2b4fc7ebf027dbb93eb4eac4613a368c99ca86
Ben Dwyer <ben@scruffian.com>
2018-06-18 12:50:39 +0100

    fix go to reference keyboard shortcuts

--- src/components/version-selector/index.js
+++ src/components/version-selector/index.js
@@ -1,6 +1,10 @@
 // External dependencies
 import React, { PropTypes } from 'react';
+import mousetrap from 'mousetrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+// Component variables
+let lastTimeStamp = 0;
 
 // Internal dependencies
 import styles from './styles.scss';
@@ -14,12 +18,66 @@ const VersionSelector = React.createClass( {
 		event.preventDefault();
 		const reference = bible.parseReference( this.refs.referenceInput.value );
 		reference.book = bible.Data.books[reference.bookID - 1][0];
-		window.location.hash = javascripture.modules.reference.createReferenceLink( reference )
+		console.log( javascripture.modules.reference.createReferenceLink( reference ) );
+		window.location.hash = javascripture.modules.reference.createReferenceLink( reference );
 		this.refs.referenceInput.blur();
 	},
 
 	componentWillReceiveProps( nextProps ) {
 		this.refs.referenceInput.value = nextProps.value;
+	},
+
+	goToNextCurrentVerse() {
+		if ( this.props.nextReference ) {
+			this.props.goToReference( this.props.nextReference );
+			this.props.markNextCurrentReference();
+		}
+	},
+
+	goToPreviousCurrentVerse() {
+		if ( this.props.previousReference ) {
+			this.props.goToReference( this.props.previousReference );
+			this.props.markPreviousCurrentReference();
+		}
+	},
+
+	goToChapter( event, combo ) {
+		const currentTimeStamp = Math.floor( event.timeStamp ),
+			currentReference = javascripture.modules.reference.getReferenceFromHash(),
+			bookId = bible.getBookId( currentReference.book );
+
+		let chapterToGoTo = combo;
+		if ( currentTimeStamp - lastTimeStamp < 750) {
+			chapterToGoTo = currentReference.chapter + combo;
+		}
+
+		if ( bible.Data.verses[bookId - 1][ chapterToGoTo - 1] ) {
+			var newReference = currentReference;
+			newReference.chapter = chapterToGoTo;
+			window.location.hash = javascripture.modules.reference.createReferenceLink( newReference );
+		}
+
+		lastTimeStamp = currentTimeStamp;
+	},
+
+	goToReferenceField( event ) {
+		this.refs.referenceInput.focus();
+		this.refs.referenceInput.selectionStart = this.refs.referenceInput.selectionEnd = 0;
+		this.refs.referenceInput.value = event.key;
+
+	},
+
+	componentDidMount() {
+		mousetrap.bind( [ '=' ], () => this.goToNextCurrentVerse( false ) );
+		mousetrap.bind( [ '-' ], () => this.goToPreviousCurrentVerse( false ) );
+		mousetrap.bind( [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ], this.goToChapter );
+		mousetrap.bind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], this.goToReferenceField );
+	},
+
+	componentWillUnmount() {
+		mousetrap.unbind( [ '=' ], () => this.goToNextCurrentVerse( false ) );
+		mousetrap.unbind( [ '-' ], () => this.goToPreviousCurrentVerse( false ) );
+		mousetrap.unbind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], this.goToReferenceField );
 	},
 
 	render() {
