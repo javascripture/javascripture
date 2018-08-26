786e3608a147a90caea4bb1dd1355c6f8d3e00de
Ben Dwyer <ben@scruffian.com>
2018-08-26 11:50:41 +0100

    allow spaces at the end of search terms

--- src/reducers/search-form.js
+++ src/reducers/search-form.js
@@ -12,12 +12,12 @@ const searchForm = ( state = initialState, action ) => {
 	switch ( action.type ) {
 		case 'UPDATE_SEARCH_FORM':
 			const updatedSearchForm = { ...state };
-			updatedSearchForm[ action.name ] = action.value.trim();
+			updatedSearchForm[ action.name ] = action.value.trimStart();
 			return updatedSearchForm;
 
 		case 'APPEND_TO_SEARCH_FORM':
 			const appendedSearchForm = { ...state };
-			appendedSearchForm[ action.name ] = ( appendedSearchForm[ action.name ] + ' ' + action.value ).trim();
+			appendedSearchForm[ action.name ] = ( appendedSearchForm[ action.name ] + ' ' + action.value ).trimStart();
 			return appendedSearchForm;
 
 		case 'CLEAR_SEARCH_FORM': 
