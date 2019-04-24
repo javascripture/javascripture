24bba0c263f64d5f389c1f5e7087097a51830be0
Ben Dwyer <ben@scruffian.com>
2019-04-24 12:14:15 +0100

    rebuild farsi bundle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1556023231';
+var cache = 'javascripture.22.0.1556104426';
 
 self.addEventListener('install', function(e) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
