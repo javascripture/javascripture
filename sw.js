091c790c193f29bb128fe4481b0634f6c0b952a7
Ben Dwyer <ben@scruffian.com>
2019-04-24 12:23:07 +0100

    clear cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1556104426';
+var cache = 'javascripture.22.0.1556104916';
 
 self.addEventListener('install', function(e) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
