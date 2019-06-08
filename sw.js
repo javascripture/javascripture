6236d53528035de54557416d8b349de60f7b5e18
Ben Dwyer <ben@scruffian.com>
2019-06-08 14:27:18 +0200

    latest build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1558817899';
+var cache = 'javascripture.22.0.1559996807';
 
 self.addEventListener('install', function(e) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
