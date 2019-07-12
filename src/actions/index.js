3f9c152c763a239fe80a8a82bfd17df831d8c0bd
Ben Dwyer <ben@scruffian.com>
2019-07-12 16:23:49 +0100

    update version number

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1562944718';
+var cache = 'javascripture.22.0.1562945016';
 
 import { createReferenceLink } from '../lib/reference.js';
 import xhr from 'xhr';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1562944718';
+var cache = 'javascripture.22.0.1562945016';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
