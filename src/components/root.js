68a747f2e31036187e798b1001f74de1013f819d
Ben Dwyer <ben@scruffian.com>
2019-11-09 22:14:33 +0000

    show the controls over the dock

--- src/components/root.js
+++ src/components/root.js
@@ -35,7 +35,7 @@ class Root extends React.Component{
 				open={ this.props.sidebar }
 				onSetOpen={this.props.toggleSidebar }
 				styles={{
-					sidebar: { background: "white", overflowY: "none", width: "320px" },
+					sidebar: { background: "white", overflowY: "none", width: "320px", zIndex: "10" },
 					overlay: { disply: "none", bottom: "auto", right: "auto" },
 				}}
 			>
