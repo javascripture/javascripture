d3ac2f10d6427915d44bf1ef52e3b1a0517930d5
Ben Dwyer <ben@scruffian.com>
2020-02-04 18:03:38 -0800

    add an accented version

--- data/bible.js
+++ data/bible.js
@@ -99,7 +99,7 @@ bible.Data.supportedVersions = {
 	'NMV': { name: 'ترجمه هزارۀ نو', language: 'fa' },
 	'AraSVD': { name: 'Arabic Bible', language: 'ar' },
 	'RomCor': { name: 'Cornilescu Bible in Romanian language', language: 'ro' },
-	'TISCH': { name: 'tishendorf', language: 'original' },
+	'ACCENTED': { name: 'Accented original', language: 'original' },
 };
 bible.Data.verses = [
 [31,25,24,26,32,22,24,22,29,32,32,20,18,24,21,16,27,33,38,18,34,24,20,67,34,35,46,22,35,43,55,32,20,31,29,43,36,30,23,23,57,38,34,34,28,34,31,22,33,26],
