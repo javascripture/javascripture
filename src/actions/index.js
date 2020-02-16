37b4fd7e242078103f5590fb204c531fa47341b7
Ben Dwyer <ben@scruffian.com>
2020-02-16 10:56:48 +0000

    force the footer over the top

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1581850354';
+var cache = 'javascripture.22.0.1581850573';
 
 import { uniq } from 'lodash';
 import xhr from 'xhr';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1581850354';
+var cache = 'javascripture.22.0.1581850573';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
