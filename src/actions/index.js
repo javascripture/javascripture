f7a4c86273ab0d4b02402389ee56eed5a0818a26
Ben Dwyer <ben@scruffian.com>
2020-02-24 21:51:01 +0000

    fix farsi book name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1582237960';
+var cache = 'javascripture.22.0.1582581012';
 
 import { uniq } from 'lodash';
 import xhr from 'xhr';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1582237960';
+var cache = 'javascripture.22.0.1582581012';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
