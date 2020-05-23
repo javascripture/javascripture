29bea1f1c4084a29bea7b398b6a3f1ea77d93c66
Ben Dwyer <ben@scruffian.com>
2020-05-23 10:45:26 +0100

    bug fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1584288696';
+var cache = 'javascripture.22.0.1590227103';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
