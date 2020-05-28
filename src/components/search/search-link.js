2383cbd8a8b0464f86d5ed3f01d41693b9b709f6
Ben Dwyer <ben@scruffian.com>
2020-05-28 16:11:37 +0100

    convert search block to functional component

--- src/components/search/search-link.js
+++ src/components/search/search-link.js
@@ -1,113 +1,81 @@
 // External dependencies
 import React from 'react';
-import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import find from 'lodash/find';
-import isEqual from 'lodash/isEqual';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Internal dependencies
 import { setCurrentVerse } from '../../actions';
 import Verse from '../reference/verse';
 import styles from './styles.scss';
-import { createReferenceLink, getReferenceText, getVerseData } from '../../lib/reference.js';
+import { createReferenceLink, getVerseData } from '../../lib/reference.js';
 import ReferenceText from '../reference-text';
 
-class SearchLink extends React.Component{
-	setCurrentVerse() {
-		const { index } = this.props;
-		this.props.setCurrentVerse( index );
-	}
-
-	expandedSearchResults = ( reference ) => {
-		if ( ! this.props.data[ this.props.terms.version ][ reference.book ][ reference.chapter - 1 ] || ! this.props.data[ this.props.terms.version ][ reference.book ][ reference.chapter - 1 ][ reference.verse - 1 ] ) {
-			console.log( 'found a non-existent verse', reference );
-			return null;
-		}
-		const verseData = getVerseData( reference, this.props.terms.version, this.props.data );
-
-		return (
-			<div className={ styles.verse }>
-				<Verse verse={ verseData } index={ null } version={ this.props.terms.version } />
-			</div>
-		);
-	};
-
-	highlightWords = () => {
-		if( ! this.props.highlightSearchResults ) {
-			return;
-		}
-
-		const { reference, terms } = this.props,
-			verseData = getVerseData( reference, terms.version ),
-			strongsNumbers = verseData.map( ( word ) => {
-				return word[ 1 ]
-			} );
-
-		window.updateAppComponent( 'highlightedWord', strongsNumbers.join( ' ' ) );
-	};
-
-	unHighlighWords = () => {
-		if( ! this.props.highlightSearchResults ) {
-			return;
-		}
-
-		window.updateAppComponent( 'highlightedWord', null );
-	};
-
-	render() {
-		const { reference, index, count } = this.props,
-			className = this.props.isActive ? styles.activeReference : null;
-
-		return (
-			<li className={ className }>
-				<a href={ '/#' + createReferenceLink( reference ) }
-					className={ styles.searchLink }
-					onClick={ () => this.setCurrentVerse( false ) }
-					onMouseOver={ this.highlightWords }
-					onMouseOut={ this.unHighlighWords }
-				>
-					{ index + 1 }. <ReferenceText reference={ reference } />
-					{ count && ' (' + count + ')' }
-				</a>
-				{ this.props.expandedSearchResults && this.expandedSearchResults( reference ) }
-			</li>
-		);
-	}
-}
-
-SearchLink.propTypes = {};
-
-function isActive( currentReference, ownProps ) {
-	if( ownProps.terms === currentReference.terms && currentReference.activeReference === ownProps.index ) {
+function isLinkActive( currentReference, index, terms ) {
+	if( terms === currentReference.terms && currentReference.activeReference === index ) {
 		return true;
 	}
 
 	return false;
 }
 
-const mapStateToProps = ( state, ownProps ) => {
-	return {
-		isActive: isActive( state.currentReference, ownProps ),
-		expandedSearchResults: state.settings.expandedSearchResults,
-		highlightSearchResults: state.settings.highlightSearchResults,
-		data: state.data,
-		interfaceLanguage: state.settings.interfaceLanguage,
+const SearchLink = React.memo( ( { reference, index, count, terms } ) => {
+	// State constants
+	const isActive = useSelector( state => isLinkActive( state.currentReference, index, terms ) );
+	const expandedSearchResults = useSelector( state => state.settings.expandedSearchResults );
+	const highlightSearchResults = useSelector( state => state.settings.highlightSearchResults );
+	const data = useSelector( state => state.data );
+
+	const dispatch = useDispatch();
+
+	// Component constants
+	const className = isActive ? styles.activeReference : null;
+	const highlightWords = () => {
+		if( ! highlightSearchResults ) {
+			return;
+		}
+
+		const verseData = getVerseData( reference, terms.version );
+		const strongsNumbers = verseData.map( ( word ) => {
+				return word[ 1 ]
+			} );
+
+		window.updateAppComponent( 'highlightedWord', strongsNumbers.join( ' ' ) );
 	};
-};
+	const unHighlighWords = () => {
+		if( ! highlightSearchResults ) {
+			return;
+		}
 
-const mapDispatchToProps = ( dispatch, ownProps ) => {
-	return {
-		setCurrentVerse: ( index ) => {
-			dispatch( setCurrentVerse( ownProps.terms, index ) );
-		},
-	}
-};
+		window.updateAppComponent( 'highlightedWord', null );
+	};
+	const expandedSearchResultsRendered = ( reference ) => {
+		if ( ! data[ terms.version ][ reference.book ][ reference.chapter - 1 ] || ! data[ terms.version ][ reference.book ][ reference.chapter - 1 ][ reference.verse - 1 ] ) {
+			console.log( 'found a non-existent verse', reference );
+			return null;
+		}
+		const verseData = getVerseData( reference, terms.version, data );
 
-const SearchLinkContainer = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)( SearchLink )
+		return (
+			<div className={ styles.verse }>
+				<Verse verse={ verseData } index={ null } version={ terms.version } />
+			</div>
+		);
+	};
 
-export default withStyles( styles )( SearchLinkContainer );
+	return (
+		<li className={ className }>
+			<a href={ '/#' + createReferenceLink( reference ) }
+				className={ styles.searchLink }
+				onClick={ () => dispatch( setCurrentVerse( terms, index ) ) }
+				onMouseOver={ highlightWords }
+				onMouseOut={ unHighlighWords }
+			>
+				{ index + 1 }. <ReferenceText reference={ reference } />
+				{ count && ' (' + count + ')' }
+			</a>
+			{ expandedSearchResults && expandedSearchResultsRendered( reference ) }
+		</li>
+	);
+} );
+
+export default withStyles( styles )( SearchLink );
