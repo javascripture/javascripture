ed144f95784873d654b56cb43fa0e909a99d278d
Ben Dwyer <ben@scruffian.com>
2020-05-28 11:41:12 +0100

    move the search results to a functional component

--- src/components/search/search-block.js
+++ src/components/search/search-block.js
@@ -1,61 +1,14 @@
 // External dependencies
 import React from 'react';
-import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { countBy, toPairs, sortBy } from 'lodash';
-import { connect } from 'react-redux';
-import find from 'lodash/find';
+import { countBy, sortBy } from 'lodash';
 import isEqual from 'lodash/isEqual';
+import { useSelector } from 'react-redux';
 
 // Internal dependencies
-import { setCurrentVerse } from '../../actions';
 import SearchLink from './search-link';
 import { getReferenceFromSearchResult } from '../../lib/reference.js';
 import styles from './styles.scss';
-
-class SearchBlock extends React.Component{
-	renderDetails() {
-		return this.props.results.map( ( result, index ) => {
-			return (
-				<SearchLink key={ index } index={ index } reference={ getReferenceFromSearchResult( result ) } terms={ this.props.terms } />
-			)
-		} );
-	}
-
-	renderSortedResults() {
-		const countedResults = countBy( this.props.results );
-		const countedResultsArray = Object.keys( countedResults ).map(key => ({ key, value: countedResults[key] }));
-		const sortedResults = sortBy( countedResultsArray, [ 'value', 'key' ] ).filter( result => result.value > 2 ).reverse();
-
-		return sortedResults.map( ( result, index ) => {
-			return <SearchLink key={ index } index={ index } reference={ getReferenceFromSearchResult( result.key ) } terms={ this.props.terms } count={ result.value } />;
-		} );
-	}
-
-	render() {
-		if ( ! this.props.results ) {
-			return ( <div className={ styles.noResults }>Loading…</div>);
-		}
-
-		if ( this.props.results.length === 0 || typeof this.props.results === 'string' ) {
-			return (
-				<div className={ this.props.open ? styles.noResults : styles.hidden }>
-					No results.
-				</div>
-			);
-		}
-
-		return (
-			<div className={ this.props.open ? null : styles.hidden }>
-				<ol className={ styles.results }>
-					{ this.props.sorted && this.renderSortedResults() || this.renderDetails() }
-				</ol>
-			</div>
-		);
-	}
-}
-
-SearchBlock.propTypes = {};
 
 function getSearchResults( searchResults, terms ) {
 	const searchResultsData = searchResults.find( searchResult => isEqual( searchResult.terms, terms ) );
@@ -65,23 +18,42 @@ function getSearchResults( searchResults, terms ) {
 	}
 }
 
-const mapStateToProps = ( state, ownProps ) => {
-	return {
-		results: getSearchResults( state.searchResults, ownProps.terms )
-	};
-};
+const SearchBlock = React.memo(( { open, sorted, terms } ) => {
+	const results = useSelector( state => getSearchResults( state.searchResults, terms ) );
 
-const mapDispatchToProps = ( dispatch, ownProps ) => {
-	return {
-		setCurrentVerse: ( index ) => {
-			dispatch( setCurrentVerse( ownProps.terms, index ) );
-		},
+	if ( ! results ) {
+		return ( <div className={ styles.noResults }>Loading…</div> );
 	}
-};
 
-const SearchBlockContainer = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)( SearchBlock )
+	if ( results.length === 0 || typeof results === 'string' ) {
+		return (
+			<div className={ open ? styles.noResults : styles.hidden }>
+				No results.
+			</div>
+		);
+	}
 
-export default withStyles( styles )( SearchBlockContainer );
+	let renderedResults;
+	if ( sorted ) {
+		const countedResults = countBy( results );
+		const countedResultsArray = Object.keys( countedResults ).map(key => ({ key, value: countedResults[key] }));
+		const sortedResults = sortBy( countedResultsArray, [ 'value', 'key' ] ).filter( result => result.value > 2 ).reverse();
+
+		renderedResults = sortedResults.map( ( result, index ) => {
+			return <SearchLink key={ index } index={ index } reference={ getReferenceFromSearchResult( result.key ) } terms={ terms } count={ result.value } />;
+		} );
+	} else {
+		renderedResults = results.map( ( result, index ) => <SearchLink key={ index } index={ index } reference={ getReferenceFromSearchResult( result ) } terms={ terms } /> )
+	}
+
+	return (
+		<div className={ open ? null : styles.hidden }>
+			<ol className={ styles.results }>
+				{ renderedResults }
+			</ol>
+		</div>
+	);
+
+} );
+
+export default withStyles( styles )( SearchBlock );
