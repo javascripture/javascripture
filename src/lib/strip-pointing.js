118134c2adaf0f16b1add5b8c5c5adb94b1fd56e
Ben Dwyer <ben@scruffian.com>
2020-06-07 21:16:30 +0100

    add greek accents to remove

--- src/lib/strip-pointing.js
+++ src/lib/strip-pointing.js
@@ -1,5 +1,7 @@
 const stripPointing = ( word ) => {
-	return word.replace(/[\u0590-\u05AF\u05BD]/g,"")
+	return word.normalize("NFD")
+		.replace(/[\u0300-\u036f]/g, "")
+		.replace(/[\u0590-\u05AF\u05BD]/g,"")
 		.replace(/[\u05B0-\u05BB]/g,"")
 		.replace(/\u05BC/g,"")
 		.replace(/\u05C1/g,"")
