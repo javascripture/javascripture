9381b562ff03e048ae1af75c5359ab2033848fa8
Ben Dwyer <ben@scruffian.com>
2020-06-20 16:47:45 +0100

    fixes

--- src/components/reference-selector-mobile/index.js
+++ src/components/reference-selector-mobile/index.js
@@ -11,7 +11,8 @@ import { closeReferenceSelectorMobile, toggleReferenceSelectorMobile, referenceS
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styles from './style.scss';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-const ReferenceSelectorMobile = ( { index, version } ) => {
+
+const ReferenceSelectorMobile = React.memo( ( { index, version } ) => {
 	const dispatch = useDispatch();
 	const inSync = useSelector( state => state.settings.inSync );
 	const references = useSelector( state => state.references );
@@ -19,8 +20,8 @@ const ReferenceSelectorMobile = ( { index, version } ) => {
 	const bookIndex = useSelector( state => state.referenceSelectorMobile[ index ].bookIndex );
 	const bookName = useSelector( state => state.referenceSelectorMobile[ index ].bookName );
 
-	const toggleList = ( event ) => {
-		event.preventDefault();
+	const toggleList = () => {
+		console.log('here');
 		dispatch( toggleReferenceSelectorMobile( index ) );
 	};
 
@@ -42,8 +43,9 @@ const ReferenceSelectorMobile = ( { index, version } ) => {
 		}
 	};
 
-	const backToBooks = () => {
-		toggleList();
+	const backToBooks = ( event ) => {
+		event.preventDefault();
+		toggleList()
 		toggleList();
 	};
 
@@ -67,7 +69,7 @@ const ReferenceSelectorMobile = ( { index, version } ) => {
 		const chapters = bible.Data.verses[ bookIndex ];
 		return (
 			<div className={ styles.chapterList }>
-				<a className={ styles.back } onClick={ backToBooks }>
+				<a className={ styles.back } onClick={ ( event ) => backToBooks( event ) }>
 					<ArrowBackIcon />
 				</a>
 				<div className={ styles.chapterName }>
@@ -117,8 +119,7 @@ const ReferenceSelectorMobile = ( { index, version } ) => {
 			</div>
 		);
 	};
-	console.log( open );
-	console.log( index );
+
 	return (
 		<span className={ styles.referenceSelectorMobile }>
 			<button type="button" className={ open ? styles.openButton : styles.button } onClick={ ( event ) => toggleList( event ) }><BookSVG /></button>
@@ -128,146 +129,6 @@ const ReferenceSelectorMobile = ( { index, version } ) => {
 			</div> }
 		</span>
 	);
-};
-
-/*class ReferenceSelectorMobile extends React.Component{
-	toggleList = ( event ) => {
-		this.props.toggleReferenceSelectorMobile( this.props.index );
-	};
-
-	openBook = ( bookName, bookIndex ) => {
-		this.props.referenceSelectorMobileSetBook( bookName, bookIndex, this.props.index );
-	};
-
-	close = () => {
-		this.props.closeReferenceSelectorMobile( this.props.index );
-	};
-
-	goToReference = ( reference ) => {
-		this.close();
-		if ( this.props.index === 0 || this.props.inSync ) {
-			window.location.hash = createReferenceLink( reference );
-		} else {
-			this.props.setReference( reference, this.props.index );
-		}
-	};
-
-	backToBooks = () => {
-		this.toggleList();
-		this.toggleList();
-	}
-
-	renderReferenceLink( book, chapter, linkText ) {
-		return (
-			<a
-				className={ styles.referenceLink }
-				key={ book + chapter }
-				onClick={ this.goToReference.bind( this, {
-					book: book,
-					chapter: chapter,
-					verse: 1
-				} ) }
-			>
-				{ linkText }
-			</a>
-		);
-	}
-
-	renderChapterList() {
-		const chapters = bible.Data.verses[ this.props.bookIndex ];
-		return (
-			<div className={ styles.chapterList }>
-				<a className={ styles.back } onClick={ this.backToBooks }>
-					<ArrowBackIcon />
-				</a>
-				<div className={ styles.chapterName }>
-					{ bible.getTranslatedBookName( this.props.bookName, this.props.version ) }
-				</div>
-				<div className={ styles.chapterBlocks }>
-					{ chapters.map( ( numberOfVerses, chapterNumber ) => {
-						return this.renderReferenceLink( this.props.bookName, chapterNumber + 1, chapterNumber + 1 );
-					} ) }
-				</div>
-			</div>
-		);
-	}
-
-	renderBookList() {
-		return (
-			<div className={ styles.bookList }>
-				<div className={ styles.bookColumn }>
-					<strong>Old</strong>
-					{ bible.Data.books.slice( 0, 39 ).map( ( book, index ) => {
-						return this.renderBookLink( book[ 0 ], index );
-					} ) }
-				</div>
-				<div className={ styles.bookColumn }>
-					<strong>New</strong>
-					{ bible.Data.books.slice( 39, 66 ).map( ( book, index ) => {
-						return this.renderBookLink( book[ 0 ], index + 39 );
-					} ) }
-				</div>
-			</div>
-		);
-	}
-
-	renderBookLink( book, index ) {
-		const chapters = bible.Data.verses[ index ];
-		const tranlatedBook = bible.getTranslatedBookName( book, this.props.version );
-		let bookLink = null;
-
-		bookLink = <div onClick={ () => this.openBook( book, index ) }> { tranlatedBook }</div>;
-		if ( chapters.length === 1 ) {
-			bookLink = this.renderReferenceLink( book, 1, tranlatedBook );
-		}
-
-		return (
-			<div key={ index } className={ styles.bookLink }>
-				{ bookLink }
-			</div>
-		);
-	}
-
-	render() {
-		return (
-			<span className={ styles.referenceSelectorMobile }>
-				<button type="button" className={ this.props.open ? styles.openButton : styles.button } onClick={ this.toggleList }><BookSVG /></button>
-				{ this.props.open && <div className={ styles.chapterSelector }>
-					{ ! this.props.bookName && this.renderBookList() }
-					{ this.props.bookName && this.renderChapterList() }
-				</div> }
-			</span>
-		);
-	}
-}
-
-const mapStateToProps = ( { reference, settings, referenceSelectorMobile }, ownProps ) => {
-	return {
-		inSync: settings.inSync,
-		references: reference,
-		open: referenceSelectorMobile[ ownProps.index ].open,
-		bookIndex: referenceSelectorMobile[ ownProps.index ].bookIndex,
-		bookName: referenceSelectorMobile[ ownProps.index ].bookName,
-	};
-};
-
-const mapDispatchToProps = ( dispatch, ownProps ) => {
-	return {
-		setReference: ( reference, index ) => {
-			dispatch( setReference( reference, index ) );
-			dispatch( setScrollChapter( reference.book, reference.chapter, index ) );
-		},
-		closeReferenceSelectorMobile: ( index ) => {
-			dispatch( closeReferenceSelectorMobile( index ) );
-		},
-		toggleReferenceSelectorMobile: ( index ) => {
-			dispatch( toggleReferenceSelectorMobile( index ) );
-		},
-		referenceSelectorMobileSetBook: ( bookName, bookIndex, index ) => {
-			dispatch( referenceSelectorMobileSetBook( bookName, bookIndex, index ) );
-		},
-	}
-};*/
-
+} );
 
 export default withStyles( styles )( ReferenceSelectorMobile );
--- src/components/trays/sidebar-controls.js
+++ src/components/trays/sidebar-controls.js
@@ -1,12 +1,11 @@
 // External dependencies
 import React from 'react';
-import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import { useDispatch, useSelector } from 'react-redux';
 import { find } from 'lodash';
 
-
 // Internal dependencies
-import { clearAll, closeSidebar } from '../../actions'
+import { clearAll, toggleSidebar } from '../../actions'
 import BookSvg from '../svg/book.js';
 import EyeSvg from '../svg/eye.js';
 import SearchSvg from '../svg/search.js';
@@ -14,7 +13,6 @@ import BookmarkSvg from '../svg/bookmark.js';
 import HelpSvg from '../svg/help.js';
 import InfoSvg from '../svg/info.js';
 import ClearSvg from '../svg/clear.js';
-import CloseSvg from '../svg/close.js';
 import styles from './styles.scss';
 import MenuOpenSvg from '../svg/menu-open.js';
 import MenuSvg from '../svg/menu.js';
@@ -28,51 +26,35 @@ const icons = {
 	InfoSvg: <InfoSvg />,
 };
 
-const SidebarControls = ( { clearAll, closeSidebar, words, icon, sidebarOpen, title } ) => (
-	<div className={ styles.sidebarControls }>
-		<span className={ styles.sidebarControlsInner }>
-			{ icons[ icon ] }
-			<span className={ styles.sidebarControlsTitle }>{ title }</span>
-		</span>
-
-		<span className={ styles.sidebarControlsRight }>
-			{ words.length > 0 && (
-				<a href="#" onClick={ clearAll } title="Clear all">
-					<ClearSvg />
-				</a>
-			) }
-			<a href="#" onClick={ closeSidebar } title="Close sidebar">
-				{ sidebarOpen ? <MenuOpenSvg /> : <MenuSvg /> }
-			</a>
-		</span>
-	</div>
-)
-
-const mapStateToProps = ( state, ownProps ) => {
-	const selectedTray = state.trays.find( tray => {
+const SidebarControls = React.memo( () => {
+	const dispatch = useDispatch();
+	const selectedTray = useSelector( state => state.trays.find( tray => {
 		return tray.visible;
-	} );
+	} ) );
+	const words = useSelector( state => state.wordDetails );
+	const sidebarOpen = useSelector( state => state.sidebar );
+	const icon = selectedTray && selectedTray.icon;
+	const title = selectedTray&& selectedTray.text;
 
-	return {
-		words: state.wordDetails,
-		icon: selectedTray && selectedTray.icon,
-		title: selectedTray&& selectedTray.text,
-		sidebarOpen: state.sidebar,
-	}
-};
+	return (
+		<div className={ styles.sidebarControls }>
+			<span className={ styles.sidebarControlsInner }>
+				{ icons[ icon ] }
+				<span className={ styles.sidebarControlsTitle }>{ title }</span>
+			</span>
 
-const mapDispatchToProps = ( dispatch ) => {
-	return {
-		clearAll: () => {
-			dispatch( clearAll() );
-		},
-		closeSidebar: () => {
-			dispatch( closeSidebar() );
-		},
-	}
-};
+			<span className={ styles.sidebarControlsRight }>
+				{ words.length > 0 && (
+					<a href="#" onClick={ () => dispatch( clearAll() ) } title="Clear all">
+						<ClearSvg />
+					</a>
+				) }
+				<a href="#" onClick={ () => dispatch( toggleSidebar() ) } title="Close sidebar" className={ styles.close }>
+					{ sidebarOpen ? <MenuOpenSvg /> : <MenuSvg /> }
+				</a>
+			</span>
+		</div>
+	);
+} );
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)( withStyles( styles )( SidebarControls ) )
+export default withStyles( styles )( SidebarControls );
