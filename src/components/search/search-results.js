73b0fcc847a82080a25af2719a60b6a7590dd4f6
Ben Dwyer <ben@scruffian.com>
2020-06-30 22:23:26 +0100

    implement the wordblockheader in search results

--- src/components/search/search-results.js
+++ src/components/search/search-results.js
@@ -1,15 +1,16 @@
 // External dependencies
-import React from 'react';
+import React, { useRef } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { useDispatch, useSelector } from 'react-redux';
 
-import CancelSvg from '../svg/cancel.js';
+// Internal dependencies
 import SearchBlock from './search-block';
 import styles from './styles.scss';
 import {
 	toggleSearch,
 	removeSearch,
 } from '../../actions'
+import WordBlockHeader from '../word-block-header';
 
 const SearchResults = () => {
 	const dispatch = useDispatch();
@@ -17,18 +18,20 @@ const SearchResults = () => {
 	const termTitle = ( { clusivity, version, lemma, morph, range, strict, word } ) => {
 		return 'word: ' + word + '\nstrongs number: ' + lemma + '\nmorphology: ' + morph + '\nversion: ' + version + '\nclusivity: ' + clusivity + '\nrange: ' + range + '\nstrict: ' + strict;
 	};
+	const textToCopy = useRef( null );
 
 	return searchTerms.map( ( searchTerm, index ) => {
 		return (
 			<div key={ index }>
-				<h2 className={ styles.header } onClick={ () => dispatch( toggleSearch( searchTerm.terms ) ) } title={ termTitle( searchTerm.terms ) }>
+				<WordBlockHeader
+					className={ styles.header }
+					title={ termTitle( searchTerm.terms ) }
+					textToCopy={ 'todo' }
+					onClick={ () => dispatch( toggleSearch( searchTerm.terms ) ) }
+					onRemove={ () => dispatch( removeSearch( searchTerm.terms ) ) }>
 					{ searchTerm.terms.word + ' ' + searchTerm.terms.lemma + ' ' + searchTerm.terms.morph }
-					<a className={ styles.remove } onClick={ () => dispatch( removeSearch( searchTerm.terms ) ) }>
-						<CancelSvg fill="#fff" />
-					</a>
-				</h2>
-
-				<SearchBlock { ...searchTerm } />
+				</WordBlockHeader>
+				<SearchBlock { ...searchTerm } ref={ textToCopy } />
 			</div>
 		);
 	} );
