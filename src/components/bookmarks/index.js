b03f03a8629f2e469db4ffb33288304d423795e4
Ben Dwyer <ben@scruffian.com>
2020-07-08 20:09:16 +0100

    make bookmarks functional

--- src/components/bookmarks/index.js
+++ src/components/bookmarks/index.js
@@ -8,17 +8,57 @@ import { removeBookmark } from '../../actions';
 import Bookmark from '../svg/bookmark.js';
 import styles from './styles.scss';
 import BookMark from './bookmark';
+import ReferenceLink from '../reference-link';
+import ReferenceText from '../reference-link';
+import { createReferenceLink } from '../../lib/reference.js';
 
-const BookMarks = ( { bookmarks, removeBookmark } ) => (
-	<div className={ styles.bookmarks }>
-		<div className={ styles.bookmarksList }>
-			{ bookmarks.length === 0 && ( <p>Click the <Bookmark /> to bookmark a verse.</p> ) }
-			{ bookmarks.map( ( bookmark, key ) => {
-				return <BookMark bookmark={ bookmark } key={ key } number={ key } />;
-			} ) }
+
+const BookMarks = React.memo( ( { bookmarks, removeBookmark } ) => {
+	const getCrossReferences = ( reference ) => {
+		const bookId = bible.getBookId( reference.book );
+		const referenceString = bible.Data.books[ bookId - 1 ][ 1 ] + '.' + reference.chapter + '.' + reference.verse;
+		return crossReferences[ referenceString ] ? crossReferences[ referenceString ] : [];
+	};
+
+	const getReferenceFromCrossReference = ( referenceString ) => {
+		const referenceArray = referenceString.split('.'),
+		bookId = bible.getBookId( referenceArray[0] ),
+		referenceObject = {
+			book: bible.Data.books[bookId - 1][0],
+			chapter: referenceArray[1],
+			verse: referenceArray[2]
+		};
+		return referenceObject;
+	};
+
+	return (
+		<div className={ styles.bookmarks }>
+			<div className={ styles.bookmarksList }>
+				{ bookmarks.length === 0 && ( <p>Click the <Bookmark /> to bookmark a verse.</p> ) }
+				{ bookmarks.map( ( bookmark, key ) => {
+					return (
+						<div key={ key }>
+							<BookMark bookmark={ bookmark } key={ key } number={ key } />
+							{ getCrossReferences( bookmark ).map( ( reference, index ) => {
+								const referenceSections = reference.split('-');
+								const referenceArrays = referenceSections.map( ( referenceSection ) => getReferenceFromCrossReference( referenceSection ) );
+
+								return (
+									<li key={ index }>
+										<a href={ '#' + createReferenceLink( referenceArrays[ 0 ] ) }>
+											{ index + 1 }. <ReferenceText reference={ referenceArrays[ 0 ] } />
+											{ referenceArrays[ 1 ] && ( <span> - <ReferenceText reference={ referenceArrays[ 1 ] } /></span> ) }
+										</a>
+									</li>
+								);
+							} ) }
+						</div>
+					);
+				} ) }
+			</div>
 		</div>
-	</div>
-)
+	)
+} );
 
 const mapStateToProps = ( state, ownProps ) => {
 	return {
