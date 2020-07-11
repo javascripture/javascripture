3a67fe0aa69fdae8b850ad973d3a13ac5284129b
Ben Dwyer <ben@scruffian.com>
2020-07-11 11:55:11 +0100

    rebuild

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1593460793';
+var cache = 'javascripture.22.0.1594464884';
 
 import { uniq } from 'lodash';
 import xhr from 'xhr';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1593460793';
+var cache = 'javascripture.22.0.1594464884';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
