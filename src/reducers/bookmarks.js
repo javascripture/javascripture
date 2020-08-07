d9a87f03ecaaacf22f2b6f2420186afe99bab932
Ben Dwyer <ben@scruffian.com>
2020-08-07 15:42:38 +0100

    refactor

--- src/reducers/bookmarks.js
+++ src/reducers/bookmarks.js
@@ -1,38 +1,38 @@
 const bookmarks = ( state = [], action ) => {
+	function referencesAreEqual( reference1, reference2 ) {
+		return reference1.book === reference2.book &&
+		reference1.chapter === reference2.chapter &&
+		reference1.verse === reference2.verse;
+	}
+
 	switch ( action.type ) {
 		case 'ADD_BOOKMARK':
-			const newState = state.filter( reference => {
-				return ! ( reference.book === action.reference.book &&
-					reference.chapter === action.reference.chapter &&
-					reference.verse === action.reference.verse );
+			const removeBookmarkFromState = state.filter( reference => {
+				return ! referencesAreEqual( reference, action.reference );
 			} );
-			const newStateClosed = newState.map( bookmark => {
+			const allBookmarksClosed = removeBookmarkFromState.map( bookmark => {
 				bookmark.open = false;
 				return bookmark;
 			} );
 			const bookmark = action.reference;
 			bookmark.open = true;
-			newStateClosed.push( bookmark );
-			return newStateClosed;
+			allBookmarksClosed.push( bookmark );
+			return allBookmarksClosed;
 
 		case 'REMOVE_BOOKMARK':
 			return state.filter( reference => {
-				return ! ( reference.book === action.reference.book &&
-					reference.chapter === action.reference.chapter &&
-					reference.verse === action.reference.verse );
+				return ! referencesAreEqual( reference, action.reference );
 			} );
 
 		case 'TOGGLE_BOOKMARK':
 			return state.map( reference => {
-				if ( reference.book === action.reference.book &&
-					reference.chapter === action.reference.chapter &&
-					reference.verse === action.reference.verse ) {
-						reference.open = ! reference.open;
-						return reference;
-					} else {
-						reference.open = false;
-						return reference;
-					}
+				if ( referencesAreEqual( reference, action.reference ) ) {
+					reference.open = ! reference.open;
+					return reference;
+				} else {
+					reference.open = false;
+					return reference;
+				}
 			} );
 
 
