1f7e77db1c55ba449ea1527eb006af80fc470845
Ben Dwyer <ben@scruffian.com>
2020-11-06 09:16:08 +0000

    rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1603042440';
+var cache = 'javascripture.22.0.1604654116';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
