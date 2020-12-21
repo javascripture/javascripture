053789a8b65d877185c37012265d316e3219ddee
Ben Dwyer <ben@scruffian.com>
2020-12-21 11:18:18 +0000

    fix reference input

--- src/components/reference-input/index.js
+++ src/components/reference-input/index.js
@@ -1,8 +1,8 @@
 // External dependencies
-import React, { Fragment } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import mousetrap from 'mousetrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { connect, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Internal dependencies
 import { setReference, setScrollChapter } from '../../actions'
@@ -11,118 +11,78 @@ import { createReferenceLink } from '../../lib/reference.js';
 // Internal dependencies
 import styles from './styles.scss';
 
-class ReferenceInput extends React.Component{
-	state = {
-		reference: this.props.value,
-	};
-
-	change = ( event ) => {
-		this.setState( {
-			reference: event.target.value,
-		} );
-	};
-
-	goToReference = ( event ) => {
-		event.preventDefault();
-		const reference = bible.parseReference( this.refs.referenceInput.value );
-		reference.book = bible.Data.books[reference.bookID - 1][0];
-		if ( this.props.index === 0 || this.props.inSync ) {
-			window.location.hash = createReferenceLink( reference );
-		} else {
-			this.props.setReference( reference, this.props.index );
-		}
-		this.refs.referenceInput.blur();
-	};
-
-	componentWillReceiveProps( nextProps ) {
-		if ( this.refs.referenceInput ) {
-			//this.refs.referenceInput.value = nextProps.value;
-			this.setState( {
-				reference: nextProps.value,
-			} );
-		}
-	}
-
-	goToReferenceField = ( event ) => {
-		event.preventDefault();
-		this.refs.referenceInput.focus();
-		this.refs.referenceInput.selectionStart = this.refs.referenceInput.selectionEnd = 0;
-		//this.refs.referenceInput.value = event.key;
-		this.setState( {
-			reference: event.key,
-		} );
-	}
-
-	componentDidMount() {
-		if ( this.props.index === 0 ) {
-			mousetrap.bind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], this.goToReferenceField );
-		}
-		if ( this.props.index === 1 ) {
-			mousetrap.bind( [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ], this.goToReferenceField );
-		}
-	}
-
-	componentWillUnmount() {
-		if ( this.props.index === 0 ) {
-			mousetrap.unbind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], this.goToReferenceField );
-		}
-		if ( this.props.index === 1 ) {
-			mousetrap.unbind( [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ], this.goToReferenceField );
-		}
-	}
-
-	render() {
-		return (
-			<div className={ styles.versionSelectorFlexible }>
-				<form onSubmit={ this.goToReference } className={ styles.versionSelectorInput} >
-					<input type="text" id="goToReference" name="reference" ref="referenceInput" placeholder="Go to reference" className={ styles.input } value={ this.state.reference } onChange={ this.change } />
-				</form>
-			</div>
-		);
-	}
-}
-
-const getBookFromState = ( state, index ) => {
-	if ( state.scrollChapter[ index ] && state.scrollChapter[ index ].book ) {
-		return state.scrollChapter[ index ].book;
+const getBookFromState = ( reference, scrollChapter ) => {
+	if ( scrollChapter && scrollChapter.book ) {
+		return scrollChapter.book;
 	} else {
-		if ( state.reference[ index ] ) {
-			return state.reference[ index ].book;
+		if ( reference ) {
+			return reference.book;
 		}
 	}
 }
-const getReferenceValue = ( state, index, version ) => {
-	const chapter = ( state.scrollChapter[ index ] && state.scrollChapter[ index ].chapter ) ? state.scrollChapter[ index ].chapter : state.reference[ index ].chapter;
-	const book = getBookFromState( state, index );
+const getReferenceValue = ( reference, scrollChapter, version ) => {
+	const chapter = ( scrollChapter && scrollChapter.chapter ) ? scrollChapter.chapter : reference.chapter;
+	const book = getBookFromState( reference, scrollChapter );
 	const tranlatedBook = bible.getTranslatedBookName( book, version );
 	return tranlatedBook + ' ' + chapter;
 };
 
-const mapStateToProps = ( state, ownProps ) => {
-	let index = state.settings.inSync ? 0 : ownProps.index;
-	// in case the references get deleted before the other columns
-	//if ( 'undefined' === typeof state.reference[ index ] ) {
-	//	index = 0;
-	//}
-	const version = state.reference[ index ].version;
-	return {
-		inSync: state.settings.inSync,
-		value: getReferenceValue( state, index, version ),
-	}
-};
+const ReferenceInput = React.memo( ( { index, last } ) => {
+	const dispatch = useDispatch();
+	const inSync = useSelector( ( state ) => state.settings.inSync );
+	const localIndex = inSync ? 0 : index;
+	const reference = useSelector( ( state ) => state.reference );
+	const scrollChapter = useSelector( ( state ) => state.scrollChapter[ localIndex ] );
+	const version = reference[ index ].version;
+	const referenceValue = getReferenceValue( reference[ localIndex ], scrollChapter, version );
+	const [ localReference, setLocalReference ] = useState( referenceValue );
+	const referenceInputField = useRef();
 
-const mapDispatchToProps = ( dispatch ) => {
-	return {
-		setReference: ( reference, index ) => {
+	const change = ( event ) => {
+		setLocalReference( event.target.value )
+	};
+
+	const goToReferenceField = ( event ) => {
+		event.preventDefault();
+		referenceInputField.current.focus();
+		referenceInputField.current.selectionStart = referenceInputField.current.selectionEnd = 0;
+		setLocalReference( event.key );
+	}
+
+	const goToReference = ( event ) => {
+		event.preventDefault();
+		const reference = bible.parseReference( localReference );
+		reference.book = bible.Data.books[reference.bookID - 1][0];
+		if ( index === 0 || inSync ) {
+			window.location.hash = createReferenceLink( reference );
+		} else {
 			dispatch( setReference( reference, index ) );
 			dispatch( setScrollChapter( reference.book, reference.chapter, index ) );
-		},
-	}
-};
+		}
+		referenceInputField.current.blur();
+	};
 
-const ReferenceInputContainer = connect(
-	mapStateToProps,
-	mapDispatchToProps,
-)( ReferenceInput );
+	useEffect( () => {
+		console.log( referenceValue );
+		setLocalReference( referenceValue );
+	}, [ reference, scrollChapter, inSync ] );
 
-export default withStyles( styles )( ReferenceInputContainer );
+	useEffect( () => {
+		if ( index === 0 ) {
+			mousetrap.bind( [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ], goToReferenceField );
+		}
+		if ( index === 1 ) {
+			mousetrap.bind( [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ], goToReferenceField );
+		}
+	} );
+
+	return (
+		<div className={ styles.versionSelectorFlexible }>
+			<form onSubmit={ goToReference } className={ styles.versionSelectorInput} >
+				<input type="text" id="goToReference" name="reference" placeholder="Go to reference" className={ styles.input } value={ localReference } onChange={ change } ref={ referenceInputField } />
+			</form>
+		</div>
+	);
+} );
+
+export default withStyles( styles )( ReferenceInput );
