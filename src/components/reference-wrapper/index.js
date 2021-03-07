1774d2b0975e83ae7e0b0a202aa93f1b79655410
Ben Dwyer <ben@scruffian.com>
2021-03-07 14:30:38 +0000

    functionalise the wrapper

--- src/components/reference-wrapper/index.js
+++ src/components/reference-wrapper/index.js
@@ -1,6 +1,6 @@
 // External
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import classnames from 'classnames';
 
 // Internal
@@ -8,38 +8,28 @@ import Reference from '../reference';
 import styles from './style.scss';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
-class ReferenceWrapper extends React.Component{
-	render() {
-		let references = this.props.references.map( ( reference, index ) => {
-			return ( <Reference reference={ reference } key={ index } index={ index } /> );
-		} );
+const ReferenceWrapper = React.memo( () => {
+	const reference = useSelector( state => state.reference );
+	const inSync = useSelector( state => state.settings.inSync );
+	const searchSelect = useSelector( state => state.searchSelect );
+	const sidebarOpen = useSelector( state => state.sidebar );
 
-		if ( this.props.inSync ) {
-			references = <Reference reference={ this.props.references[ 0 ] } index={ 0 } />
-		}
+	let references = reference.map( ( singleReference, index ) => {
+		return ( <Reference reference={ singleReference } key={ index } index={ index } /> );
+	} );
 
-		const className = classnames( styles.referenceWrapper, this.props.sidebarOpen ? styles.referenceWrapperSidebarOpen : null, this.props.searchSelect ? 'search-select' : null );
-		return (
-			<div className={ className }>
-				<div className={ styles.referenceWrapperInner }>
-					{ references }
-				</div>
+	if ( inSync ) {
+		references = <Reference reference={ reference[ 0 ] } index={ 0 } />
+	}
+
+	const className = classnames( styles.referenceWrapper, sidebarOpen ? styles.referenceWrapperSidebarOpen : null, searchSelect ? 'search-select' : null );
+	return (
+		<div className={ className }>
+			<div className={ styles.referenceWrapperInner }>
+				{ references }
 			</div>
-		);
-	}
-}
+		</div>
+	);
+} );
 
-const mapStateToProps = ( { reference, settings, searchSelect, sidebar }, ownProps ) => {
-	return {
-		inSync: settings.inSync,
-		references: reference,
-		searchSelect,
-		sidebarOpen: sidebar,
-	}
-};
-
-const ReferenceWrapperContainer = connect(
-	mapStateToProps,
-)( ReferenceWrapper )
-
-export default withStyles( styles )( ReferenceWrapperContainer );
+export default withStyles( styles )( ReferenceWrapper );
