5e0fa60a15c8e9cd7ca29f049421adb5a047c6ee
Ben Dwyer <ben@scruffian.com>
2021-03-21 12:48:34 +0000

    refactor reference info to a functional component

--- src/components/trays/reference-info.js
+++ src/components/trays/reference-info.js
@@ -1,6 +1,6 @@
 // External dependencies
-import React, { Fragment } from 'react';
-import { connect, useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
@@ -15,28 +15,24 @@ import {
 
 import styles from './styles.scss';
 
-const ReferenceInfoInner = React.memo( ( props ) => {
+const ReferenceInfo = React.memo( ( props ) => {
 	const dispatch = useDispatch();
+	const reference = useSelector( state => state.referenceInfo.reference );
+	const referenceToCompareWith = useSelector( state => state.referenceInfo.referenceToCompareWith );
 	const overlap = useSelector( state => compareTwoReferences( state ) );
 	const rare = useSelector( state => calculateRareWords( state ) );
 	const common = useSelector( state => calculateCommonWords( state ) );
+	const limit = useSelector( state => state.referenceInfo.limit );
 	const addAllWords = () => {
-		overlap.forEach( lemma => {
-			dispatch( setTrayVisibilityFilter( 'word' ) );
-			dispatch( selectWord( {
-				lemma,
-				version: 'original',
-			} ) );
-		} );
+		overlap.forEach( lemma => addWord( lemma ) );
 	};
-
-	const getWord = ( lemma ) => {
-		return (
-			<div>
-				{ lemma } - { javascripture.data.strongsDictionary[ lemma ].lemma } - { javascripture.data.strongsDictionary[ lemma ].xlit }
-			</div>
-		);
-	}
+	const addWord = ( lemma ) => {
+		dispatch( setTrayVisibilityFilter( 'word' ) );
+		dispatch( selectWord( {
+			lemma,
+			version: 'original',
+		} ) );
+	};
 
 	const getOverlap = () => {
 		if ( ! overlap ) {
@@ -55,7 +51,7 @@ const ReferenceInfoInner = React.memo( ( props ) => {
 				{ overlapMarkup }
 			</div>
 		)
-	}
+	};
 
 	const getCommonWords = () => {
 		if ( ! common ) {
@@ -75,8 +71,136 @@ const ReferenceInfoInner = React.memo( ( props ) => {
 			);
 		} );
 	};
+
+	const bookChange = ( event ) => {
+		dispatch( setReferenceInfoCompareWith( { book: event.target.value, chapter: 1, verse: 'all' } ) );
+	};
+
+	const chapterChange = ( event ) => {
+		dispatch( setReferenceInfoCompareWith( { ...referenceToCompareWith, chapter: event.target.value, verse: 'all' } ) );
+	};
+
+	const verseChange = ( event ) => {
+		dispatch( setReferenceInfoCompareWith( { ...referenceToCompareWith, verse: event.target.value } ) );
+	};
+
+	const getChapters = () => {
+		if ( referenceToCompareWith && referenceToCompareWith.book ) {
+			const bookNumber = bible.getBookId( referenceToCompareWith.book );
+			return bible.Data.verses[ bookNumber - 1 ].map( ( verses, index ) => <option key={ index }>{ index + 1 }</option> );
+		}
+
+		return <option>-</option>;
+	};
+
+	const getVerses = ( reference ) => {
+		if ( reference && reference.book && reference.chapter ) {
+			const bookNumber = bible.getBookId( reference.book );
+			const numberOfVerses = bible.Data.verses[ bookNumber - 1 ][ reference.chapter - 1 ];
+			const verses = [];
+			for ( var i = 0 ; i < numberOfVerses ; i++) {
+				verses.push( i );
+			}
+			const versesJSX = verses.map( ( key ) => {
+				return <option key={ key }>{ key + 1 }</option>
+			} );
+			versesJSX.unshift( <option key="all" value="all">All</option> );
+			return versesJSX;
+		}
+
+		return <option>-</option>;
+	};
+
+	const addAllRareWords = () => {
+		rare.forEach( lemma => addWord( lemma ) );
+	};
+
+	const getRareWords = () => {
+		if ( ! rare ) {
+			return null;
+		}
+
+		if ( rare.length === 0 ) {
+			return 'No rare words found';
+		}
+
+		return rare.map( lemma => <div key={ lemma }>{ getWord( lemma ) }</div> );
+	};
+
+	const getWord = ( lemma ) => {
+		return (
+			<div>
+				{ lemma } - { javascripture.data.strongsDictionary[ lemma ].lemma } - { javascripture.data.strongsDictionary[ lemma ].xlit }
+			</div>
+		);
+	};
+
+	const changeLimit = (event) => dispatch( setReferenceInfoLimit( event.target.value ) );
+
+	const compareBookChange = ( event ) => {
+		dispatch( setReferenceInfo( { book: event.target.value, chapter: 1, verse: 'all' } ) );
+	};
+
+	const compareChapterChange = ( event ) => {
+		dispatch( setReferenceInfo( { ...reference, chapter: event.target.value, verse: 'all' } ) );
+	};
+
+	const compareVerseChange = ( event ) => {
+		dispatch( setReferenceInfo( { ...reference, verse: event.target.value } ) );
+	};
+
+	const getCompareChapters = () => {
+		if ( reference && reference.book ) {
+			const bookNumber = bible.getBookId( reference.book );
+			return bible.Data.verses[ bookNumber - 1 ].map( ( verses, index ) => <option key={ index }>{ index + 1 }</option> );
+		}
+
+		return <option>-</option>;
+	};
+
+	const getBooks = () => {
+		return (
+			<React.Fragment>
+				<option value="">Select a book</option>
+				{ bible.Data.books.map( book => <option key={ book[ 0 ] }>{ book[0] }</option> ) }
+			</React.Fragment>
+		);
+	};
+
 	return (
-		<Fragment>
+		<div className={ styles.chapterTrayPadding }>
+			<div className={ styles.chapterTray }>
+				<select name="compareWithBook" name="compareWithBook" onChange={ compareBookChange } value={ reference ? reference.book : '' }>
+					{ getBooks() }
+				</select>
+				<select name="compareWithChapter" name="compareWithChapter" onChange={ compareChapterChange } value={ reference ? reference.chapter : '' }>
+					{ getCompareChapters() }
+				</select>
+				<select name="compareWithVerses" onChange={ compareVerseChange } value={ reference ? reference.verse : '' }>{ getVerses( reference ) }</select>
+			</div>
+			<br />
+			<h2>Rare words</h2>
+			<div className={ styles.chapterTray }>
+				Words used less than <input type="number" name="limit" value={ limit } onChange={ changeLimit } className={ styles.limit } /> times { rare ? '(' + rare.length + ')' : null }:
+			</div>
+			<div className={ styles.scrollingBlock }>
+				{ getRareWords() }
+			</div>
+			<div className={ styles.chapterTray }>
+				{ rare && rare.length > 0 && <button onClick={ addAllRareWords }>Select all rare words (slow!)</button> }
+			</div>
+			<br />
+			<h2>Compare with</h2>
+			<div className={ styles.chapterTray }>
+				<select name="book" onChange={ bookChange } value={ referenceToCompareWith ? referenceToCompareWith.book : '' }>
+					{ getBooks() }
+				</select>
+				<select name="chapter" onChange={ chapterChange } value={ referenceToCompareWith ? referenceToCompareWith.chapter : '' }>{ getChapters() }</select>
+				<select name="verses" onChange={ verseChange } value={ referenceToCompareWith? referenceToCompareWith.verse : '' }>{ getVerses( referenceToCompareWith ) }</select>
+			</div>
+			<div className={ styles.chapterTray }>
+				For words used less than <input type="number" name="limit" value={ limit } onChange={ changeLimit } className={ styles.limit } /> times.
+			</div>
 			<div className={ styles.scrollingBlock }>
 				{ getOverlap() }
 			</div>
@@ -88,186 +212,9 @@ const ReferenceInfoInner = React.memo( ( props ) => {
 			<div className={ styles.scrollingBlock }>
 				{ getCommonWords() }
 			</div>
-		</Fragment>
-	)
+		</div>
+	);
 } );
 
-class ReferenceInfo extends React.Component {
-	bookChange = ( event ) => {
-		this.props.setReferenceInfoCompareWith( { book: event.target.value, chapter: 1, verse: 'all' } );
-	}
 
-	chapterChange = ( event ) => {
-		this.props.setReferenceInfoCompareWith( { ...this.props.referenceToCompareWith, chapter: event.target.value, verse: 'all' } );
-	}
-
-	verseChange = ( event ) => {
-		this.props.setReferenceInfoCompareWith( { ...this.props.referenceToCompareWith, verse: event.target.value } );
-	}
-
-	getChapters() {
-		if ( this.props.referenceToCompareWith && this.props.referenceToCompareWith.book ) {
-			const bookNumber = bible.getBookId( this.props.referenceToCompareWith.book );
-			return bible.Data.verses[ bookNumber - 1 ].map( ( verses, index ) => <option key={ index }>{ index + 1 }</option> );
-		}
-
-		return <option>-</option>;
-	}
-
-	getVerses( reference ) {
-		if ( reference && reference.book && reference.chapter ) {
-			const bookNumber = bible.getBookId( reference.book );
-			const numberOfVerses = bible.Data.verses[ bookNumber - 1 ][ reference.chapter ];
-			const verses = [];
-			for ( var i = 0 ; i <= numberOfVerses ; i++) {
-				verses.push( i );
-			}
-			const versesJSX = verses.map( ( key ) => {
-				return <option key={ key }>{ key + 1 }</option>
-			} );
-			versesJSX.unshift( <option key="all" value="all">All</option> );
-			return versesJSX;
-		}
-
-		return <option>-</option>;
-	}
-
-	compareBookChange = ( event ) => {
-		this.props.setReferenceInfo( { book: event.target.value, chapter: 1, verse: 'all' } );
-	}
-
-	compareChapterChange = ( event ) => {
-		this.props.setReferenceInfo( { ...this.props.reference, chapter: event.target.value, verse: 'all' } );
-	}
-
-	compareVerseChange = ( event ) => {
-		this.props.setReferenceInfo( { ...this.props.reference, verse: event.target.value } );
-	}
-
-	getCompareChapters() {
-		if ( this.props.reference && this.props.reference.book ) {
-			const bookNumber = bible.getBookId( this.props.reference.book );
-			return bible.Data.verses[ bookNumber - 1 ].map( ( verses, index ) => <option key={ index }>{ index + 1 }</option> );
-		}
-
-		return <option>-</option>;
-	}
-
-	changeLimit = ( event ) => {
-		this.props.setReferenceInfoLimit( event.target.value );
-	};
-
-	addAllRareWords = () => {
-		this.props.rare.forEach( lemma => this.props.addWord( lemma ) );
-	};
-
-	getRareWords() {
-		if ( ! this.props.rare ) {
-			return null;
-		}
-
-		if ( this.props.rare.length === 0 ) {
-			return 'No rare words found';
-		}
-
-		return this.props.rare.map( lemma => <div key={ lemma }>{ this.getWord( lemma ) }</div> );
-	}
-
-	getWord( lemma ) {
-		return (
-			<div>
-				{ lemma } - { javascripture.data.strongsDictionary[ lemma ].lemma } - { javascripture.data.strongsDictionary[ lemma ].xlit }
-			</div>
-		);
-	}
-
-	render() {
-		return (
-			<div className={ styles.chapterTrayPadding }>
-				<div className={ styles.chapterTray }>
-					<select name="compareWithBook" name="compareWithBook" onChange={ this.compareBookChange } value={ this.props.reference ? this.props.reference.book : '' }>
-						<option value="">Select a book</option>
-						{
-							bible.Data.books.map( book => <option key={ book[ 0 ] }>{ book[0] }</option> )
-						}
-					</select>
-					<select name="compareWithChapter" name="compareWithChapter" onChange={ this.compareChapterChange } value={ this.props.reference ? this.props.reference.chapter : '' }>
-						{ this.getCompareChapters() }
-					</select>
-					<select name="compareWithVerses" onChange={ this.compareVerseChange } value={ this.props.reference ? this.props.reference.verse : '' }>{ this.getVerses( this.props.reference ) }</select>
-				</div>
-				<br />
-
-				<h2>Rare words</h2>
-				<div className={ styles.chapterTray }>
-					Words used less than <input type="number" name="limit" value={ this.props.limit } onChange={ this.changeLimit } className={ styles.limit } /> times { this.props.rare ? '(' + this.props.rare.length + ')' : null }:
-				</div>
-				<div className={ styles.scrollingBlock }>
-					{ this.getRareWords() }
-				</div>
-				<div className={ styles.chapterTray }>
-					{ this.props.rare && this.props.rare.length > 0 && <button onClick={ this.addAllRareWords }>Select all rare words (slow!)</button> }
-				</div>
-				<br />
-				<h2>Compare with</h2>
-				<div className={ styles.chapterTray }>
-					<select name="book" onChange={ this.bookChange } value={ this.props.referenceToCompareWith ? this.props.referenceToCompareWith.book : '' }>
-						<option value="">Select a book</option>
-						{
-							bible.Data.books.map( book => <option key={ book[ 0 ] }>{ book[0] }</option> )
-						}
-					</select>
-					<select name="chapter" onChange={ this.chapterChange } value={ this.props.referenceToCompareWith ? this.props.referenceToCompareWith.chapter : '' }>{ this.getChapters() }</select>
-					<select name="verses" onChange={ this.verseChange } value={ this.props.referenceToCompareWith? this.props.referenceToCompareWith.verse : '' }>{ this.getVerses( this.props.referenceToCompareWith ) }</select>
-				</div>
-				<div className={ styles.chapterTray }>
-					For words used less than <input type="number" name="limit" value={ this.props.limit } onChange={ this.changeLimit } className={ styles.limit } /> times.
-				</div>
-				<ReferenceInfoInner />
-			</div>
-		);
-	}
-}
-
-ReferenceInfo.propTypes = {};
-
-const mapStateToProps = ( state ) => {
-	return {
-		reference: state.referenceInfo.reference,
-		referenceToCompareWith: state.referenceInfo.referenceToCompareWith,
-		limit: state.referenceInfo.limit,
-		overlap: compareTwoReferences( state ),
-		rare: calculateRareWords( state ),
-		common: calculateCommonWords( state ),
-		data: state.data,
-		connectionQuality: null, //calculateConnectionQuality( state ),
-	}
-}
-
-const mapDispatchToProps = ( dispatch, ownProps ) => {
-	return {
-		addWord: ( lemma ) => {
-			dispatch( setTrayVisibilityFilter( 'word' ) );
-			dispatch( selectWord( {
-				lemma,
-				version: 'original',
-			} ) );
-		},
-		setReferenceInfo: ( reference ) => {
-			dispatch( setReferenceInfo( reference ) );
-		},
-		setReferenceInfoCompareWith: ( reference ) => {
-			dispatch( setReferenceInfoCompareWith( reference ) );
-		},
-		setReferenceInfoLimit: ( limit ) => {
-			dispatch( setReferenceInfoLimit( limit ) );
-		},
-	}
-};
-
-const ConnectedReferenceInfo = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)( ReferenceInfo )
-
-export default withStyles( styles )( ConnectedReferenceInfo );
+export default withStyles( styles )( ReferenceInfo );
