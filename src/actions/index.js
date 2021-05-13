04d615d7b8b422ebda66fc384d4436c2acf9669c
Ben Dwyer <ben@scruffian.com>
2021-05-13 23:23:26 +0100

    Fix textual issues in the greek

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1619196141';
+var cache = 'javascripture.22.0.1620944572';
 import xhr from 'xhr';
 import { createReferenceLink, getAllLemmasFromReference } from '../lib/reference.js';
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1619196141';
+var cache = 'javascripture.22.0.1620944572';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
