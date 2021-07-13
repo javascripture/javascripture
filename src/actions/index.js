2c49193de5f5605700d019437044759904f14dfb
Ben Dwyer <ben@scruffian.com>
2021-07-13 23:27:09 +0100

    rebuild

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1624393829';
+var cache = 'javascripture.22.0.1626215212';
 import xhr from 'xhr';
 import { createReferenceLink, getAllLemmasFromReference } from '../lib/reference.js';
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1624393829';
+var cache = 'javascripture.22.0.1626215212';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
